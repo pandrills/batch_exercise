5f8ee9928047f20f8a1eed3475001c2907f0df52
Muyi Adesote <olu.adesote@yahoo.co.uk>
2018-06-06 11:56:26 +0100

    second commit

--- src/main/java/homeoffice/page_factory/read_vehicle_reg_detailsPage.java
+++ src/main/java/homeoffice/page_factory/read_vehicle_reg_detailsPage.java
@@ -4,6 +4,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.springframework.util.Assert;
 
 /**
  * Created by musti on 06/06/2018.
@@ -20,18 +21,36 @@ public class read_vehicle_reg_detailsPage {
     private WebElement StartNow;
 
     @FindBy (xpath = "//*[@id='Vrm']")
-    private WebElement EngterReg;
+    private WebElement EnterReg;
 
     @FindBy (xpath = "//*[@name='Continue']")
     private WebElement ContinueBtn;
 
-    @FindBy (xpath = "//*[@class='reg-mark']")
+    @FindBy (xpath = "//*[@id='pr3']/div/ul/li[1]/span[2]")  //*[@class='reg-mark']
     private WebElement CarReg;
 
-    @FindBy (xpath = "//*[@class='reg-mark']")
+    @FindBy (xpath = "//*[@id='pr3']/div/ul/li[2]/span[2]/strong")  //*[@class='reg-mark']
     private WebElement CarMake;
 
-    @FindBy (xpath = "//*[@class='reg-mark']")
+    @FindBy (xpath = "//*[@id=pr3']/div/ul/li[3]/span[2]/strong")
     private WebElement CarColour;
 
+
+    public void startSearch(){
+        StartNow.click();
+    }
+
+    public void enterCarReg(String id){
+        EnterReg.sendKeys(id);
+    }
+
+    public void clickContinue(){
+        ContinueBtn.click();
+    }
+
+    public void verifyCarId() {
+         System.out.println(CarReg.getText());
+         System.out.println(CarReg.isDisplayed());
+
+    }
 }
--- src/test/java/homeoffice/MyStepdefs.java
+++ src/test/java/homeoffice/MyStepdefs.java
@@ -1,11 +1,16 @@
 package homeoffice;
 
 import cucumber.api.java.en.*;
+import homeoffice.page_factory.read_vehicle_reg_detailsPage;
+import org.springframework.util.Assert;
 
 /**
  * Created by musti on 06/06/2018.
  */
 public class MyStepdefs extends DriverInstance{
+
+    read_vehicle_reg_detailsPage rvp = new read_vehicle_reg_detailsPage(driver);
+
     @Given("^I am on the Get vehicle information from DVLA page$")
     public void iAmOnTheGetVehicleInformationFromDVLAPage() throws Throwable {
         // Write code here that turns the phrase above into concrete actions
@@ -28,6 +33,7 @@ public class MyStepdefs extends DriverInstance{
 
     @Then("^the details of the vehicle should be correctly displayed$")
     public void the_details_of_the_vehicle_should_be_correctly_displayed() throws Throwable {
+        rvp.verifyCarId();
 
     }
 }
